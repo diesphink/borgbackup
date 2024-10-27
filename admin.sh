a954c667de18faf248adf176467e5fcb94257ef5
Diego Pereyra <diesphink@gmail.com>
2024-10-26 21:57:04 -0300

    Primeira versão

---
+++ admin.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Stops execution in case of error
+set -e
+
+# Load environment variables from variables file on current script directory
+source "$(dirname "$0")/variables"
+
+# Create log folder if it doesn't exist
+if [ ! -d $LOG_FOLDER ]; then
+  mkdir -p $LOG_FOLDER
+fi
+
+# Set command to the first argument, or to "help" if no argument is provided
+COMMAND=${1:-help}
+
+# Repository key file
+KEY_FILE=$(whoami)-$(hostname).key
+
+# If command is init
+if [ "$COMMAND" = "init" ]; then
+  # Initialize the repository
+  echo -n "1. Inicializando repositório..."
+  borg init --encryption=repokey $BORG_REPO >> $LOG_FILE 2>&1
+  echo " done."
+
+  echo -ne "2. Exportando chave..."
+  borg key export $BORG_REPO > $KEY_FILE
+  echo " done."
+
+  echo
+  echo -e "============================"
+  echo 
+  echo "Atenção: Salve em local seguro tanto a passphrase quanto o arquivo $KEY_FILE"
+  echo
+  echo "============================"
+
+  exit 0
+fi
+
+# If command is check
+if [ "$COMMAND" = "check" ]; then
+  # Check the repository
+  borg check $BORG_REPO >> $LOG_FILE 2>&1
+  exit 0
+fi
+
+# If command is prune
+if [ "$COMMAND" = "prune" ]; then
+  # Prune the repository
+  borg prune --list $BORG_PRUNE_OPTIONS $BORG_REPO >> $LOG_FILE 2>&1
+  exit 0
+fi
---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Stops execution in case of error
+set -e
+
+# Load environment variables from variables file on current script directory
+source "$(dirname "$0")/variables"
+
+# Create log folder if it doesn't exist
+if [ ! -d $LOG_FOLDER ]; then
+  mkdir -p $LOG_FOLDER
+fi
+
+# Run the backup
+borg create \
+  --filter "AME" \
+  --list \
+  --stats \
+  --verbose \
+  --exclude-caches \
+  --patterns-from patterns \
+  ::$BORG_ARCHIVE_NAME \
+  $BORG_SOURCE_PATH 2>&1 | tee -a $LOG_FILE
+
+# Prune the repository
+borg prune --list $BORG_PRUNE_OPTIONS $BORG_REPO 2>&1 | tee -a $LOG_FILE
