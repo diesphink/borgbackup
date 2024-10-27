c97c0ded50aaf18a27d7589a76206f52a7e4219e
Diego Pereyra <diesphink@gmail.com>
2024-10-26 22:26:14 -0300

    Instalar como serviço systemd

--- admin.sh
+++ admin.sh
@@ -51,3 +51,36 @@ if [ "$COMMAND" = "prune" ]; then
   borg prune --list $BORG_PRUNE_OPTIONS $BORG_REPO >> $LOG_FILE 2>&1
   exit 0
 fi
+
+# If command is install
+if [ "$COMMAND" = "install" ]; then
+  # Prune the repository
+  echo "1. Copiando arquivos de serviço..."
+  sudo cp -v "$(dirname "$0")/systemd/borg-backup.service" /etc/systemd/system/borg-backup.service
+  sudo cp -v "$(dirname "$0")/systemd/borg-backup.timer" /etc/systemd/system/borg-backup.timer
+  echo " done."
+
+  echo "2. Recarregando serviços..."
+  sudo systemctl daemon-reload
+  echo " done."
+
+  echo "3. Habilitando timer..."
+  sudo systemctl enable --now borg-backup.timer
+  echo " done."
+
+  exit 0
+fi
+
+# If all else, show help
+echo "Uso: $0 [init|check|prune|install]"
+echo
+echo "  init    Inicializa o repositório"
+echo "  check   Verifica a integridade do repositório"
+echo "  prune   Remove backups antigos"
+echo "  install Instala o serviço de backup com timer para 2h30"
+echo
+echo "Sem argumentos, exibe esta mensagem de ajuda."
+echo
+echo "Para mais informações, consulte o README.md"
+echo
+
